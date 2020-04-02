590574efda2fa7dcb856ab6cafc8c9aa25052ba2
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2020-04-02 21:41:35 +0200

    test solver in sandbox

--- test/cpp/sandbox.cpp
+++ test/cpp/sandbox.cpp
@@ -1,4 +1,26 @@
-#include<iostream>
+#include <iostream>
+
+#include "datatypes.hpp"
+#include "dynamicequation.hpp"
+#include "ferromagnet.hpp"
+#include "grid.hpp"
+#include "timesolver.hpp"
+#include "world.hpp"
+
 int main() {
-    return 0;
+  World world({1.0, 1.0, 1.0});
+  Ferromagnet* magnet = world.addFerromagnet("my_magnet", Grid({8, 8, 1}));
+  DynamicEquation llg(magnet->magnetization(), magnet->torque());
+  TimeSolver solver(llg, 1e-9);
+  for (int i = 0; i < 1410; i++) {
+      std::cout << i << std::endl;
+    solver.step();
+  }
+
+  for (int i = 0; i < 1414; i++) {
+      std::cout << i << std::endl;
+    solver.step();
+  }
+
+  return 0;
 }
