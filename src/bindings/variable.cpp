b2c2ec677214ae73314d8407489503ff4193d06b
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2020-04-02 19:40:15 +0200

    Let python Variable inherit from python Quantity

--- src/bindings/variable.cpp
+++ src/bindings/variable.cpp
@@ -4,13 +4,10 @@
 
 #include "field.hpp"
 #include "wrappers.hpp"
+#include "quantity.hpp"
 
 void wrap_variable(py::module& m) {
-  py::class_<Variable>(m, "Variable")
-      .def_property_readonly("name", &Variable::name)
-      .def_property_readonly("unit", &Variable::unit)
-      .def_property_readonly("ncomp", &Variable::ncomp)
-      .def_property_readonly("grid", &Variable::grid)
+  py::class_<Variable, Quantity>(m, "Variable")
       .def("get", [](const Variable* v) { return fieldToArray(v->field()); })
       .def("set", [](const Variable* v, py::array_t<real> data) {
         std::unique_ptr<Field> tmp(new Field(v->grid(), v->ncomp()));
