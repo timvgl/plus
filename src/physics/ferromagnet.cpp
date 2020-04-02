013970572eca2d30792088e5c86db8edd287bd4d
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2020-04-02 11:04:02 +0200

    initialize magnetization wih random values

--- src/physics/ferromagnet.cpp
+++ src/physics/ferromagnet.cpp
@@ -1,5 +1,7 @@
 #include "ferromagnet.hpp"
 
+#include <random>
+
 Ferromagnet::Ferromagnet(World* world, std::string name, Grid grid)
     : System(world, name, grid),
       demagField_(this),
@@ -7,7 +9,28 @@ Ferromagnet::Ferromagnet(World* world, std::string name, Grid grid)
       exchangeField_(this),
       effectiveField_(this),
       torque_(this),
-      magnetization_(name + ":magnetization", "", 3, grid) {}
+      magnetization_(name + ":magnetization", "", 3, grid) {
+  aex = 0;
+  anisU = {0, 0, 0};
+  msat = 1.0;
+  ku1 = 0.0;
+  alpha = 0;
+
+  // TODO: this can be done much more efficient somewhere else
+  {
+    int ncomp = 3;
+    int nvalues = ncomp * grid_.ncells();
+    std::vector<real> randomValues(nvalues);
+    std::uniform_real_distribution<real> unif(-1, 1);
+    std::default_random_engine randomEngine;
+    for (auto& v : randomValues) {
+      v = unif(randomEngine);
+    }
+    Field randomField(grid_, ncomp);
+    randomField.setData(&randomValues[0]);
+    magnetization_.set(&randomField);
+  }
+}
 
 Ferromagnet::~Ferromagnet() {}
 
