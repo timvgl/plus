980878a85c1c32d64fa80f8762187fd5c3cf27f8
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2020-04-10 11:24:17 +0200

    remove test.py

--- examples/test.py
+++
@@ -1,9 +0,0 @@
-from mumax5 import World, Grid, anisotropyField
-
-w = World(cellsize=(1e-9,1e-9,1e-9))
-
-magnet = w.addFerromagnet(name='magnet', grid=Grid((10,10,1)))
-
-f = anisotropyField(magnet)
-
-print(f.get())
