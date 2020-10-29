62b3788cc2ef9bf94045391bcb79c8c78f9c6702
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2020-10-29 17:25:43 +0100

    bench multiple grid sizes

--- examples/bench.py
+++ examples/bench.py
@@ -1,22 +1,55 @@
 from mumax5 import *
+
+import matplotlib.pyplot as plt
 import time
 
-world = World((4e-9, 4e-9, 4e-9))
 
-magnet = Ferromagnet(world, Grid((128, 64, 1)))
+def simple_bench(grid, nsteps=100):
+    """Returns the walltime of a simple simulation using the specified grid
+    and numbers of steps"""
+
+    world = World((4e-9, 4e-9, 4e-9))
+
+    magnet = Ferromagnet(world, grid)
+    magnet.msat = 800e3
+    magnet.aex = 13e-12
+    magnet.alpha = 0.5
+
+    solver = TimeSolver(magnet.magnetization, magnet.torque)
+    solver.timestep = 1e-13
+    solver.adaptive_timestep = False
+
+    solver.steps(10)  # warm up
+
+    start = time.time()
+    solver.steps(nsteps)
+    stop = time.time()
+
+    return stop-start
 
 
-magnet.msat = 800e3
-magnet.aex = 13e-12
-magnet.alpha = 0.5
+if __name__ == "__main__":
+    NSTEPS = 100
 
-magnet.magnetization = (1, 0.1, 0)
+    ncells = []
+    throughputs = []
 
-solver = TimeSolver(magnet.magnetization, magnet.torque)
-solver.timestep = 1e-13
-solver.adaptive_timestep = False
+    print("\n{:>10} {:>10} {:>12}".format("ncells", "walltime", "throughput"))
 
-nsteps = 1000
-start = time.time()
-solver.steps(nsteps)
-print("#steps/s: ", round(nsteps/(time.time()-start)))
+    for p in range(2, 12):
+        grid = Grid((2**p, 2**p, 1))
+        walltime = simple_bench(grid, NSTEPS)
+        throughput = grid.ncells*NSTEPS/walltime
+
+        print("{:>10} {:>10.5f} {:>12.3E}".format(
+            grid.ncells, walltime, throughput))
+
+        ncells.append(grid.ncells)
+        throughputs.append(throughput)
+
+    print()
+
+    plt.loglog(ncells, throughputs, '-o')
+    plt.xlabel("Number of cells")
+    plt.ylabel("Throughput")
+    plt.show()
