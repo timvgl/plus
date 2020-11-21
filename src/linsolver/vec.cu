b03c7432fdd14f9cf3d8461f86beff2eeccf3b5b
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2020-11-21 09:44:16 +0100

    check GVec sizes in GVec operations

--- src/linsolver/vec.cu
+++ src/linsolver/vec.cu
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 #include "cudalaunch.hpp"
 #include "vec.hpp"
 
@@ -21,7 +23,11 @@ __global__ void k_add(real* y, real a1, real* x1, real a2, real* x2, int N) {
 }
 
 GVec add(real a1, const GVec& x1, real a2, const GVec& x2) {
-  // TODO: return error if size does not match
+  if (x1.size() != x2.size()) {
+    throw std::invalid_argument(
+        "Arrays can not be added together because their dimensions do not "
+        "match");
+  }
   int N = x1.size();
   GVec y(N);
   cudaLaunch(N, k_add, y.get(), a1, x1.get(), a2, x2.get(), N);
@@ -51,7 +57,6 @@ __global__ void k_maxAbsValue(real* result, real* x, int N) {
         sdata[tid] = sdata[tid + s];
     __syncthreads();
   }
-  // TODO: check if loop unrolling makes sense here
 
   // Set the result
   if (tid == 0)
@@ -59,6 +64,9 @@ __global__ void k_maxAbsValue(real* result, real* x, int N) {
 }
 
 real maxAbsValue(const GVec& x) {
+  if (x.size() == 0)
+    return 0.0;
+
   real* d_result = (real*)bufferPool.allocate(sizeof(real));
   cudaLaunchReductionKernel(k_maxAbsValue, d_result, x.get(), (int)x.size());
 
@@ -93,11 +101,19 @@ __global__ void k_dotSum(real* result, real* f, real* g, int N) {
     *result = sdata[0];
 }
 
-real dotSum(const GVec& f, const GVec& g) {
-  // todo: check size
+real dotSum(const GVec& x1, const GVec& x2) {
+  if (x1.size() != x2.size()) {
+    throw std::invalid_argument(
+        "Arrays can not be added together because their dimensions do not "
+        "match");
+  }
+
+  if (x1.size() == 0)
+    return 0.0;
+
   real* d_result = (real*)bufferPool.allocate(sizeof(real));
-  cudaLaunchReductionKernel(k_dotSum, d_result, f.get(), g.get(),
-                            (int)f.size());
+  cudaLaunchReductionKernel(k_dotSum, d_result, x1.get(), x2.get(),
+                            (int)x1.size());
   // copy the result to the host and return
   real result;
   checkCudaError(cudaMemcpyAsync(&result, d_result, sizeof(real),
