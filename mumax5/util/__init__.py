7438d97a650489e835696b357b98220e1c35bc34
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2020-11-27 16:19:55 +0100

    add configs in util/__init__.py

--- mumax5/util/__init__.py
+++ mumax5/util/__init__.py
@@ -1,6 +1,13 @@
 """Utilities for mumax5."""
 
-from .config import neelskyrmion
+from .config import antivortex, blochskyrmion, neelskyrmion, vortex
 from .show import show_field, show_layer
 
-__all__ = ["neelskyrmion", "show_field", "show_layer"]
+__all__ = [
+    "vortex",
+    "antivortex",
+    "neelskyrmion",
+    "blochskyrmion",
+    "show_field",
+    "show_layer",
+]
