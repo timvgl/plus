419b79a32425cbd7b5330c249090008e1b515cf8
Ian Lateur <ian.lateur@ugent.be>
2024-07-03 16:57:30 +0200

    Add boundary conditions to DMI tests

--- test/mumax3/test_mumax3_interfacialdmi.py
+++ test/mumax3/test_mumax3_interfacialdmi.py
@@ -1,15 +1,20 @@
-from mumax3 import Mumax3Simulation
 import numpy as np
+import pytest
+from mumax3 import Mumax3Simulation
 
 from mumax5 import Ferromagnet, Grid, World
 
 RTOL = 1e-3
 
+@pytest.fixture(params=[True, False])
+def openbc(request):
+    return request.param
 
 class TestInterfacialDMI:
     """Test interfacial induced dmi against mumax3."""
 
-    def setup_class(self):
+    @pytest.fixture(autouse=True)
+    def setup_class(self, openbc):
         # arbitrarily chosen parameters
         msat, dind = 800e3, 3e-3
         cellsize = (1e-9, 2e-9, 3.2e-9)
@@ -24,8 +29,8 @@ class TestInterfacialDMI:
                 m = randommag()
                 saveas(m, "m.ovf")
                 
-                // default in mumax3 is false, mumax5 uses open bc
-                openbc = true 
+                // default in mumax3 is false, these tests use both
+                openbc = {openbc}
 
                 // The dmi is included in the exchange in mumax3
                 // because Aex is set to zero here, b_exch is the dmi field
@@ -38,36 +43,37 @@ class TestInterfacialDMI:
         self.world = World((1e-9, 2e-9, 3.2e-9))
         self.magnet = Ferromagnet(self.world, Grid(gridsize))
         self.magnet.enable_demag = False
+        self.magnet.enable_openbc = openbc
         self.magnet.msat = msat
         self.magnet.dmi_tensor.set_interfacial_dmi(dind)
         self.magnet.magnetization.set(self.mumax3sim.get_field("m"))
 
-    def test_dmi_field(self):
+    def test_dmi_field(self, openbc):
         wanted = self.mumax3sim.get_field("b_dmi")
         result = self.magnet.dmi_field()
         assert np.allclose(result, wanted, rtol=RTOL)
 
-    def test_dmi_in_effective_field(self):
+    def test_dmi_in_effective_field(self, openbc):
         wanted = self.magnet.dmi_field()
         result = self.magnet.effective_field()
         assert np.allclose(result, wanted, rtol=RTOL)
 
-    def test_dmi_energy_density(self):
+    def test_dmi_energy_density(self, openbc):
         wanted = self.mumax3sim.get_field("edens_dmi")
         result = self.magnet.dmi_energy_density()
         assert np.allclose(result, wanted, rtol=RTOL)
 
-    def test_dmi_in_total_energy_density(self):
+    def test_dmi_in_total_energy_density(self, openbc):
         wanted = self.magnet.dmi_energy_density()
         result = self.magnet.total_energy_density()
         assert np.allclose(result, wanted, rtol=RTOL)
 
-    def test_dmi_energy(self):
+    def test_dmi_energy(self, openbc):
         wanted = self.mumax3sim.get_field("e_dmi").flat[0]  # same value in all cells
         result = self.magnet.dmi_energy()
         assert np.isclose(result, wanted, rtol=RTOL)
 
-    def test_dmi_in_total_energy(self):
+    def test_dmi_in_total_energy(self, openbc):
         wanted = self.magnet.dmi_energy()
         result = self.magnet.total_energy()
         assert np.isclose(result, wanted, rtol=RTOL)
