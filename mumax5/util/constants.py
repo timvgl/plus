1f78122a4e47ee74d75e1f134bceb7993040b635
Ian Lateur <ian.lateur@ugent.be>
2024-07-18 13:21:13 +0200

    Add physical constants on Python side

---
+++ mumax5/util/constants.py
@@ -0,0 +1,17 @@
+"""Physical constants used in mumax5.
+This file copies src/physics/constants.hpp."""
+
+# Gyromagnetic ratio in rad/Ts
+GAMMALL = 1.7595E11
+# Reduced gyromagnetic ratio in Hz/T
+GAMMA0 = 2.8024951E10
+# Vacuum permeability in H/m
+MU0 = 1.25663706212E-6
+# Boltzmann constant in J/K
+KB = 1.38064852E-23
+# Electron charge in C
+QE = 1.60217646E-19
+# Bohr magneton in J/T
+MUB = 9.2740091523E-24
+# Reduced Planck constant in Js
+HBAR = 1.054571817E-34
