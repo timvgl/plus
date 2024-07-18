cf8644df0de4377d4a883569e37678555c7221ef
Ian Lateur <ian.lateur@ugent.be>
2024-07-18 17:49:58 +0200

    Start formulary

--- mumax5/util/__init__.py
+++ mumax5/util/__init__.py
@@ -2,6 +2,7 @@
 
 from .constants import GAMMALL, GAMMA0, MU0, KB, QE, MUB, HBAR
 from .config import twodomain, antivortex, blochskyrmion, neelskyrmion, vortex
+from .formulary import magnetostatic_energy_density, Km, exchange_length, l_ex, wall_width, helical_length, magnetic_hardness
 from .show import show_field, show_layer, show_neel, show_magnet_geometry, show_field_3D
 from .shape import *
 
@@ -12,6 +13,8 @@ __all__ = [
     "antivortex",
     "neelskyrmion",
     "blochskyrmion",
+    "magnetostatic_energy_density", "Km", "exchange_length", "l_ex",
+    "wall_width", "helical_length", "magnetic_hardness",
     "show_field",
     "show_layer",
     "show_magnet_geometry",
