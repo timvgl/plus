161d4d9950573dcd79bcfa9877aefab5d2fc482b
Ian Lateur <ian.lateur@ugent.be>
2024-07-18 13:21:13 +0200

    Add physical constants on Python side

--- mumax5/util/__init__.py
+++ mumax5/util/__init__.py
@@ -1,10 +1,12 @@
 """Utilities for mumax5."""
 
+from .constants import GAMMALL, GAMMA0, MU0, KB, QE, MUB, HBAR
 from .config import twodomain, antivortex, blochskyrmion, neelskyrmion, vortex
 from .show import show_field, show_layer, show_neel, show_magnet_geometry, show_field_3D
 from .shape import *
 
 __all__ = [
+    "GAMMALL", "GAMMA0", "MU0", "KB", "QE", "MUB", "HBAR",
     "twodomain",
     "vortex",
     "antivortex",
