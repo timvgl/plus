d5d8b0e79a9294b893f2799a8d09f2f3622566a2
Ian Lateur <ian.lateur@ugent.be>
2024-08-12 17:38:27 +0200

    Remove strange sub2 function from ScalarQuantity

--- src/core/scalarquantity.hpp
+++ src/core/scalarquantity.hpp
@@ -8,7 +8,6 @@ class ScalarQuantity {
  public:
   virtual ~ScalarQuantity();
   virtual real eval() const = 0;
-  virtual bool sub2() const;
   virtual std::string name() const;
   virtual std::string unit() const;
 };
