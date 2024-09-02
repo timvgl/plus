4461c6e9c7718bceb1d8815a0e0e8082b6c11a34
lamoreel <moreelslars@gmail.com>
2024-09-02 13:58:45 +0200

    Add more user functionalities

--- mumaxplus/util/voronoi.py
+++ mumaxplus/util/voronoi.py
@@ -4,17 +4,31 @@ import _mumaxpluscpp as _cpp
 class VoronoiTesselator:
 
     def __init__(self, world, grid, grainsize):
-        cellsize = world.cellsize
-        self._impl = _cpp.VoronoiTesselator(grid._impl, grainsize, cellsize)
-
-    def generate(self):
-        return self._impl.generate()
-    
-    def centers(self):
-        """lijst of tuples (idx, coo)"""
-        return 0
-    
-    def indexdict(self):
-        "dictionary with key: idx and values (all coo belonging to idx)"
-        return 0
+        self._impl = _cpp.VoronoiTesselator(grid._impl, grainsize, world.cellsize)
         
+    def generate(self):
+        """Generate a Voronoi tesselation."""
+        self.tesselation = self._impl.generate()
+        return self.tesselation[0]
+    
+    def indexDictionary(self):
+        """Create a dictionary where each coordinate (value) is linked
+        to a region index (key)."""
+        from collections import defaultdict
+        _, nz, ny, nx = self.tesselation.shape
+        
+        idxs = self.tesselation[0].flatten()
+        coords = _np.array(_np.meshgrid(range(nx), range(ny), range(nz), indexing='ij')
+                          ).reshape(3, -1).T
+        
+        idxDict = defaultdict(list)
+        
+        for idx, coord in zip(idxs, coords):
+            idxDict[idx].append(tuple(coord))
+            
+        idxDict = dict(idxDict)
+        return idxDict
+
+    def numRegions(self):
+        """Returns number of unique region indices."""
+        return _np.unique(_np.ravel(self.tesselation)).size
--- src/bindings/wrap_voronoi.cpp
+++ src/bindings/wrap_voronoi.cpp
@@ -12,6 +12,5 @@ void wrap_voronoi(py::module& m) {
     py::class_<VoronoiTesselator>(m, "VoronoiTesselator")
 
         .def(py::init<Grid, real, real3>(), py::arg("grid"), py::arg("grainsize"), py::arg("cellsize"))
-
         .def("generate", [](VoronoiTesselator t) { return fieldToArray(t.generate()); });
 }
