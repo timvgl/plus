fd941adcaeb4aae47ecc525e123e607a818e9fc6
Diego <diego.degusem@ugent.be>
2024-09-17 17:38:20 +0200

    Create an example for error scaling

---
+++ examples/errorscaling.py
@@ -0,0 +1,97 @@
+"""This script compares numerical and analytical result of one spin after one precession
+in an external magnetic field of 0.1 T. This comparison is done for different time steps
+in order to recreate figure 10 of the paper "The design and verification of MuMax3".
+However with different algorithms.
+https://doi.org/10.1063/1.4899186 """
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from mumaxplus import *
+from mumaxplus.util import *
+
+
+def single_system(method, dt):
+    """This function simulates a single spin in a magnetic field of 0.1 T without damping.
+
+    Returns the absolute error between the simulation and the exact solution.
+
+    Parameters:
+    method -- The used simulation method
+    dt     -- The time step
+    """
+    # --- Setup ---
+    world = World(cellsize=(1e-9, 1e-9, 1e-9))
+    
+    hfield_z = 0.1  # External field strength
+    duration = 2*np.pi/(GAMMALL * hfield_z)  # Time of one precession
+
+    magnet = Ferromagnet(world, grid=Grid((1, 1, 1)))
+    magnet.enable_demag = False
+    magnet.magnetization = (1/np.sqrt(2), 0, 1/np.sqrt(2))
+    magnet.alpha = 0.
+    magnet.aex = 10e-12
+    magnet.msat = 1/MU0
+    world.bias_magnetic_field = (0, 0, hfield_z)
+
+    # --- Run the simulation ---
+    world.timesolver.set_method(method)
+    world.timesolver.adaptive_timestep = False
+    world.timesolver.timestep = dt
+    
+    world.timesolver.run(duration)
+    output = magnet.magnetization.average()
+
+    # --- Compare with exact solution ---
+    exact = np.array([1/np.sqrt(2), 0., 1/np.sqrt(2)])
+    error = np.linalg.norm(exact - output)
+
+    return error
+
+
+method_names = ["Heun", "BogackiShampine", "CashKarp", "Fehlberg", "DormandPrince"]
+exact_order = {"Heun": 2,
+               "BogackiShampine": 3,
+               "CashKarp": 5,
+               "Fehlberg": 5,
+               "DormandPrince": 6
+               }
+
+N = 41  # Amount of data points
+dts = {"Heun": np.logspace(-12, np.log10(0.9e-10), N), 
+       "BogackiShampine": np.logspace(np.log10(0.25e-11), np.log10(0.9e-10), N),
+       "CashKarp": np.logspace(np.log10(1.5e-11), np.log10(0.9e-10), N),
+       "Fehlberg": np.logspace(np.log10(1.5e-11), np.log10(0.9e-10), N),
+       "DormandPrince": np.logspace(np.log10(1.5e-11), np.log10(0.9e-10), N)
+       }
+
+# --- Plotting ---
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim((0.9e-12, 0.1e-9))
+plt.ylim((1e-7, 1))
+plt.xlabel("time steps (s)")
+plt.ylabel("absolute error after 1 precession")
+
+# --- Simulation Loops ---
+orders = {}
+for method in method_names:
+    error = np.zeros(shape=dts[method].shape)
+    for i, dt in enumerate(dts[method]):
+        err = single_system(method, dt)
+        error[i] = err
+    
+    # Find the order
+    log_dts, log_error = np.log10(dts[method]), np.log10(error)
+    order = np.polyfit(log_dts, log_error, 1)[0]
+    orders[method] = order
+
+    plt.scatter(dts[method], error, marker="o", zorder=2)
+
+    intercept = np.polyfit(log_dts, log_error - log_dts * exact_order[method], 0)
+    plt.plot(np.array([1e-14, 1e-9]), (10**intercept)*np.array([1e-14, 1e-9])**exact_order[method], label=method,)
+
+# print(orders)  # Uncomment if you want to see the estimated orders
+plt.legend()
+plt.show()
