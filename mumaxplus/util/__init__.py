ae3c98f45062dfeb27d6dab90f1c9bc5dfa4c537
Ian Lateur <ian.lateur@gmail.com>
2024-09-30 14:32:17 +0200

    Remove GAMMA0 and add comment

--- mumaxplus/util/__init__.py
+++ mumaxplus/util/__init__.py
@@ -1,6 +1,6 @@
 """Utilities for mumaxplus."""
 
-from .constants import GAMMALL, GAMMA0, MU0, KB, QE, MUB, HBAR
+from .constants import GAMMALL, MU0, KB, QE, MUB, HBAR
 from .config import twodomain, antivortex, blochskyrmion, neelskyrmion, vortex
 from .formulary import magnetostatic_energy_density, Km, exchange_length, l_ex, wall_width, helical_length, magnetic_hardness
 from .show import show_field, show_layer, show_magnet_geometry, show_field_3D
@@ -8,7 +8,7 @@ from .shape import *
 from .voronoi import VoronoiTessellator
 
 __all__ = [
-    "GAMMALL", "GAMMA0", "MU0", "KB", "QE", "MUB", "HBAR",
+    "GAMMALL", "MU0", "KB", "QE", "MUB", "HBAR",
     "twodomain",
     "vortex",
     "antivortex",
