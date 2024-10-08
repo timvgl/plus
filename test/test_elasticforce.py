1506b68f30dd08b3cc216a7d875ee6b1ba4800c5
Ian Lateur <ian.lateur@gmail.com>
2024-10-08 12:39:07 +0200

    Remove repetition in test_elasticforce

--- test/test_elasticforce.py
+++ test/test_elasticforce.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from mumaxplus import Grid, World, Ferromagnet
 
 
+SRTOL = 1e-3
+
 def max_absolute_error(result, wanted):
     """Maximum error for vector quantities."""
     return np.max(np.linalg.norm(result - wanted, axis=0))
@@ -18,10 +20,52 @@ def max_semirelative_error(result, wanted):
     return max_absolute_error(result, wanted) / np.max(abs(wanted))
 
 
-cx, cy, cz = 2e-9, 2e-9, 2e-9  # TODO: make different
+def make_long_magnet(d_comp):
+    """Makes a world with a magnet of the appropriate size according to the
+    direction of the double derivative.
+    """
+    gridsize, pbc_repetitions = [0, 0, 0], [0, 0, 0]
+    gridsize[d_comp], pbc_repetitions[d_comp] = N1, 1  # set for PBC grid
+    world = World(cellsize, mastergrid=Grid(gridsize), pbc_repetitions=pbc_repetitions)
+
+    gridsize[(d_comp + 1)%3], gridsize[(d_comp + 2)%3] = n1, n2  # set for magnet
+    magnet =  Ferromagnet(world, Grid(gridsize))
+    magnet.enable_elastodynamics = True
+
+    return magnet
+
+
+def set_and_check_sine_force(magnet, d_comp, u_comp, C):
+    """Sets given component of the displacement of the given magnet to a sine
+    wave, the periodicity of which depends on the direction of the derivative.
+    Then checks if the simulated force corresponds to the expected analitical force.
+    You need to set C yourself.
+    """
+    magnet.enable_elastodynamics = True  # just in case
+
+    L = N1*cellsize[d_comp]
+    k = P * 2*math.pi/L
+    def displacement_func(x, y, z):
+        u = [0., 0., 0.]
+        relevant_coord = (x, y, z)[d_comp]
+        u[u_comp] = A * math.sin(k * relevant_coord)
+        return tuple(u)
+
+    magnet.elastic_displacement = displacement_func
+    
+    force_num = magnet.elastic_force.eval()
+    force_anal = - k**2 * C * magnet.elastic_displacement.eval()
+
+    assert max_semirelative_error(force_num, force_anal) < SRTOL
+
+
+cx, cy, cz = 1.5e-9, 2e-9, 2.5e-9
 cellsize = (cx, cy, cz)
+N1, N2, n1, n2 = 128, 64, 1, 4
 P = 2  # sinus periods
 A = 1e-15  # displacement amplitude
+c11 = 283e9
+c44 = 58e9
 
 
 # ==================================================
@@ -29,116 +73,70 @@ A = 1e-15  # displacement amplitude
 # c11 is constant TODO: vary c11 as well
 
 def test_dx_dx_ux():
-    nx, ny, nz = 128, 1, 1
-    length, width, thickness = nx*cx, ny*cy, nz*cz
-
-    world = World(cellsize, mastergrid=Grid((nx, 0, 0)), pbc_repetitions=(1,0,0))
-    magnet = Ferromagnet(world, Grid((nx, ny, nz)))
-    
-    magnet.enable_elastodynamics = True
-    c11 = 283e9
+    magnet = make_long_magnet(d_comp=0)
     magnet.c11 = c11
-
-    k = P * 2*math.pi/length
-    magnet.elastic_displacement = lambda x,y,z: (A * math.sin(k * x), 0, 0)
-
-    force_num = magnet.elastic_force.eval()
-    force_anal = - k**2 * c11 * magnet.elastic_displacement.eval()
-
-    assert max_semirelative_error(force_num, force_anal) < 1e-3
-
+    set_and_check_sine_force(magnet, d_comp=0, u_comp=0, C=c11)
 
 def test_dy_dy_uy():
-    nx, ny, nz = 1, 128, 1
-    length, width, thickness = nx*cx, ny*cy, nz*cz
-
-    world = World(cellsize, mastergrid=Grid((0, ny, 0)), pbc_repetitions=(0,1,0))
-    magnet = Ferromagnet(world, Grid((nx, ny, nz)))
-    
-    magnet.enable_elastodynamics = True
-    c11 = 283e9
+    magnet = make_long_magnet(d_comp=1)
     magnet.c11 = c11
-
-    k = P * 2*math.pi/width
-    magnet.elastic_displacement = lambda x,y,z: (0, A * math.sin(k * y), 0)
-
-    force_num = magnet.elastic_force.eval()
-    force_anal = - k**2 * c11 * magnet.elastic_displacement.eval()
-
-    assert max_semirelative_error(force_num, force_anal) < 1e-3
-
+    set_and_check_sine_force(magnet, d_comp=1, u_comp=1, C=c11)
 
 def test_dz_dz_uz():
-    nx, ny, nz = 1, 1, 128
-    length, width, thickness = nx*cx, ny*cy, nz*cz
-
-    world = World(cellsize, mastergrid=Grid((0, 0, nz)), pbc_repetitions=(0,0,1))
-    magnet = Ferromagnet(world, Grid((nx, ny, nz)))
-    
-    magnet.enable_elastodynamics = True
-    c11 = 283e9
+    magnet = make_long_magnet(d_comp=2)
     magnet.c11 = c11
-
-    k = P * 2*math.pi/thickness
-    magnet.elastic_displacement = lambda x,y,z: (0, 0, A * math.sin(k * z))
-
-    force_num = magnet.elastic_force.eval()
-    force_anal = - k**2 * c11 * magnet.elastic_displacement.eval()
-
-    assert max_semirelative_error(force_num, force_anal) < 1e-3
-
+    set_and_check_sine_force(magnet, d_comp=2, u_comp=2, C=c11)
 
 # ==================================================
 # Tests for double derivate along the different direction
 # c44 is constant TODO: vary c44 as well (careful of mixed derivatives!)
 
-
 def test_dy_dy_ux():
-    nx, ny, nz = 1, 128, 1
-    length, width, thickness = nx*cx, ny*cy, nz*cz
-
-    world = World(cellsize, mastergrid=Grid((0, ny, 0)), pbc_repetitions=(0,1,0))
-    magnet = Ferromagnet(world, Grid((nx, ny, nz)))
-    
-    magnet.enable_elastodynamics = True
-    c44 = 58e9
+    magnet = make_long_magnet(d_comp=1)
     magnet.c44 = c44
     magnet.c12 = -c44  # to remove mixed derivate
-
-    k = P * 2*math.pi/width
-    magnet.elastic_displacement = lambda x,y,z: (A * math.sin(k * y), 0, 0)
-
-    force_num = magnet.elastic_force.eval()
-    force_anal = - k**2 * c44 * magnet.elastic_displacement.eval()
-
-    assert max_semirelative_error(force_num, force_anal) < 1e-3
-
+    set_and_check_sine_force(magnet, d_comp=1, u_comp=0, C=c44)
 
 def test_dz_dz_ux():
-    nx, ny, nz = 1, 1, 128
-    length, width, thickness = nx*cx, ny*cy, nz*cz
-
-    world = World(cellsize, mastergrid=Grid((0, 0, nz)), pbc_repetitions=(0,0,1))
-    magnet = Ferromagnet(world, Grid((nx, ny, nz)))
-    
-    magnet.enable_elastodynamics = True
-    c44 = 58e9
+    magnet = make_long_magnet(d_comp=2)
     magnet.c44 = c44
     magnet.c12 = -c44  # to remove mixed derivate
-
-    k = P * 2*math.pi/thickness
-    magnet.elastic_displacement = lambda x,y,z: (A * math.sin(k * z), 0, 0)
-
-    force_num = magnet.elastic_force.eval()
-    force_anal = - k**2 * c44 * magnet.elastic_displacement.eval()
-
-    assert max_semirelative_error(force_num, force_anal) < 1e-3
+    set_and_check_sine_force(magnet, d_comp=2, u_comp=0, C=c44)
 
 
-# TODO: test much more!
+def test_dx_dx_uy():
+    magnet = make_long_magnet(d_comp=0)
+    magnet.c44 = c44
+    magnet.c12 = -c44  # to remove mixed derivate
+    set_and_check_sine_force(magnet, d_comp=0, u_comp=1, C=c44)
+
+def test_dz_dz_uy():
+    magnet = make_long_magnet(d_comp=2)
+    magnet.c44 = c44
+    magnet.c12 = -c44  # to remove mixed derivate
+    set_and_check_sine_force(magnet, d_comp=2, u_comp=1, C=c44)
+
+
+def test_dx_dx_uz():
+    magnet = make_long_magnet(d_comp=0)
+    magnet.c44 = c44
+    magnet.c12 = -c44  # to remove mixed derivate
+    set_and_check_sine_force(magnet, d_comp=0, u_comp=2, C=c44)
+
+def test_dy_dy_uz():
+    magnet = make_long_magnet(d_comp=1)
+    magnet.c44 = c44
+    magnet.c12 = -c44  # to remove mixed derivate
+    set_and_check_sine_force(magnet, d_comp=1, u_comp=2, C=c44)
+
+
+# ==================================================
+# Tests for mixed derivates
+# f_i += (c12+c44) ∂j(∂i(u_j))
+# c12+c44 is constant TODO: vary c12+c44 as well (careful of double derivative!)
+# TODO: make the tests
 
 
 if __name__ == "__main__":
     # for manual testing
-    test_dy_dy_ux()
-    test_dz_dz_ux()
+    pass
