80d77ae2220db2addac6b96f444d2b0cfc2661e1
Ian Lateur <ian.lateur@gmail.com>
2024-10-09 11:03:28 +0200

    Add mixed derivative tests for ellastic force

--- test/test_elasticforce_uniform_c.py
+++ test/test_elasticforce_uniform_c.py
@@ -1,3 +1,11 @@
+"""This file tests every term of the elastic force individually by comparing
+the calculated elastic force to the analytical solutions.
+Periodic boundry conditions are used in the direction(s) corresponding to the
+derivative direction(s) so everything stays smooth, while free boundary
+conditions are assumed in all other directions (so no effect).
+All stiffness constants are uniform in these tests for simplicity!
+"""
+
 import pytest
 import numpy as np
 import math
@@ -8,6 +16,17 @@ from mumaxplus import Grid, World, Ferromagnet
 
 
 SRTOL = 1e-3
+SRTOL_MIX = 1e-2  # mixed derivative tests are less precise
+
+cx, cy, cz = 1.5e-9, 2e-9, 2.5e-9
+cellsize = (cx, cy, cz)
+N1, N2, n1, n2 = 128, 256, 1, 4
+P1, P2 = 2, 3  # number of sinus periods
+A = 1e-15  # displacement amplitude
+c11 = 283e9
+c44 = 58e9
+c12 = 166e9
+
 
 def max_absolute_error(result, wanted):
     """Maximum error for vector quantities."""
@@ -38,13 +57,13 @@ def make_long_magnet(d_comp):
 def set_and_check_sine_force(magnet, d_comp, u_comp, C):
     """Sets given component of the displacement of the given magnet to a sine
     wave, the periodicity of which depends on the direction of the derivative.
-    Then checks if the simulated force corresponds to the expected analitical force.
+    Then checks if the simulated force corresponds to the expected analytical force.
     You need to set C yourself.
     """
     magnet.enable_elastodynamics = True  # just in case
 
     L = N1*cellsize[d_comp]
-    k = P * 2*math.pi/L
+    k = P1 * 2*math.pi/L
     def displacement_func(x, y, z):
         u = [0., 0., 0.]
         relevant_coord = (x, y, z)[d_comp]
@@ -57,15 +76,6 @@ def set_and_check_sine_force(magnet, d_comp, u_comp, C):
     force_anal = - k**2 * C * magnet.elastic_displacement.eval()
 
     assert max_semirelative_error(force_num, force_anal) < SRTOL
-
-
-cx, cy, cz = 1.5e-9, 2e-9, 2.5e-9
-cellsize = (cx, cy, cz)
-N1, N2, n1, n2 = 128, 64, 1, 4
-P = 2  # sinus periods
-A = 1e-15  # displacement amplitude
-c11 = 283e9
-c44 = 58e9
 
 
 # ==================================================
@@ -132,9 +142,84 @@ def test_dy_dy_uz():
 
 # ==================================================
 # Tests for mixed derivates
-# f_i += (c12+c44) ∂j(∂i(u_j))
-# c12+c44 is constant TODO: vary c12+c44 as well (careful of double derivative!)
-# TODO: make the tests
+# f_i += c12 ∂j(∂i(u_j))
+# c12 is constant TODO: vary c12 as well (no c44 so no double derivative!)
+
+def analytical_mixed_force(k_outer, k_inner, d_comp_outer, d_comp_inner, mgrid):
+    force = np.zeros_like(mgrid)
+    force[d_comp_inner, ...] = A * c12 * k_outer * k_inner * \
+                                np.cos(k_inner * mgrid[d_comp_inner]) *\
+                                np.cos(k_outer * mgrid[d_comp_outer])
+    return force
+
+def check_mixed_derivative(d_comp_outer, d_comp_inner):
+    """Makes a world with a rectangular magnet of the appropriate size according
+    to the directions of the mixed derivatives.
+
+    Then sets the d_comp_outer component of the displacement of the given magnet
+    to a product of sine waves, the periodicity of which depends on the direction
+    of the derivatives.
+
+    Then checks if the simulated force corresponds to the expected analytical
+    force at d_comp_inner.
+    """
+    
+    # make world
+    gridsize, pbc_repetitions = [0, 0, 0], [0, 0, 0]
+    gridsize[d_comp_outer], gridsize[d_comp_inner] = N1, N2
+    pbc_repetitions[d_comp_outer], pbc_repetitions[d_comp_inner] = 1, 1
+    world = World(cellsize, mastergrid=Grid(gridsize), pbc_repetitions=pbc_repetitions)
+
+    # make magnet
+    gridsize = [n1, n1, n1]  # other index will stay n1
+    gridsize[d_comp_outer], gridsize[d_comp_inner] = N1, N2
+    magnet =  Ferromagnet(world, Grid(gridsize))
+    magnet.enable_elastodynamics = True
+
+    magnet.c12 = c12  # enabling only mixed derivative
+
+    # set displacement to A * sin(ki * i) * sin(kj * j)
+    L_outer = N1*cellsize[d_comp_outer]
+    L_inner = N2*cellsize[d_comp_inner]
+    k_outer = P1 * 2*math.pi/L_outer
+    k_inner = P2 * 2*math.pi/L_inner
+
+    def displacement_func(x, y, z):
+        u = [0., 0., 0.]
+        outer_coord = (x, y, z)[d_comp_outer]
+        inner_coord = (x, y, z)[d_comp_inner]
+        u[d_comp_outer] = A * math.sin(k_outer * outer_coord) * \
+                              math.sin(k_inner * inner_coord)
+        return tuple(u)
+
+    magnet.elastic_displacement = displacement_func
+
+    # compare forces
+    force_num = magnet.elastic_force.eval()
+    force_anal = analytical_mixed_force(k_outer, k_inner,
+                                        d_comp_outer, d_comp_inner,
+                                        mgrid=magnet.elastic_force.meshgrid)
+
+    assert max_semirelative_error(force_num, force_anal) < SRTOL_MIX
+
+
+def test_dy_dx_uy():
+    check_mixed_derivative(d_comp_outer=1, d_comp_inner=0)
+
+def test_dz_dx_uz():
+    check_mixed_derivative(d_comp_outer=2, d_comp_inner=0)
+
+def test_dx_dy_ux():
+    check_mixed_derivative(d_comp_outer=0, d_comp_inner=1)
+
+def test_dz_dy_uz():
+    check_mixed_derivative(d_comp_outer=2, d_comp_inner=1)
+
+def test_dx_dz_ux():
+    check_mixed_derivative(d_comp_outer=0, d_comp_inner=2)
+
+def test_dx_dz_ux():
+    check_mixed_derivative(d_comp_outer=1, d_comp_inner=2)
 
 
 if __name__ == "__main__":
