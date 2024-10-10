1844f220679fae99090d80f0a46951e7378e5f6b
Diego <diego.degusem@ugent.be>
2024-10-10 12:47:25 +0200

    Make test clearer and less redundant

--- test/test_magnetoelasticfield.py
+++ test/test_magnetoelasticfield.py
@@ -9,7 +9,7 @@ RTOL = 1e-4
 cx, cy, cz = 1.5e-9, 2e-9, 2.5e-9
 cellsize = (cx, cy, cz)
 P = 2  # periods
-A = 1  # amplitude
+A = 4  # amplitude
 N = 128  # number of 1D cells
 msat = 800e3
 B = -8.8e6
@@ -43,10 +43,10 @@ def create_magnet(d_comp, m_comp):
     magnet.magnetization = m
     magnet.msat = msat
 
-    return world, magnet
+    return magnet
 
 
-def sine_displacement(magnet, d_comp, cos_comp, B1, B2):
+def sine_displacement(magnet, i_comp, j_comp, B1, B2):
     """Creates the magnetization following a sine in the d_comp direction
     and cosine in another direction it then calculates the magnetoelasticforce.
     """
@@ -55,141 +55,96 @@ def sine_displacement(magnet, d_comp, cos_comp, B1, B2):
     magnet.B1 = B1
     magnet.B2 = B2
 
-    L = N*cellsize[d_comp]
+    L = N*cellsize[i_comp]
     k = P*2*math.pi/L
 
     def displacement_func(x, y, z):
         u = [0,0,0]
-        comp = (x,y,z)[d_comp]
-        u[d_comp] = A * math.sin(k * comp)
-        u[cos_comp] = A * math.cos(k * comp)
+        comp = (x,y,z)[i_comp]
+        u[j_comp] = A * math.sin(k * comp)
         return tuple(u)
 
     magnet.elastic_displacement = displacement_func
     strain_num = magnet.strain_tensor.eval()
     
     strain_anal = np.zeros(shape=strain_num.shape)
-    strain_anal[d_comp,...] = k * magnet.elastic_displacement.eval()[cos_comp,...]
-    strain_anal[2 + d_comp + cos_comp,...] = - 0.5 * k * magnet.elastic_displacement.eval()[d_comp,...]
 
-    h_num = magnet.magnetoelastic_field.eval()
-    h_anal = np.zeros(shape=h_num.shape)
+    # derivative of sine is cosine
+    cos = np.zeros(shape=strain_anal[0,...].shape)
+    index = [0, 0, 0]
+    index[i_comp] = slice(None)  # index with length N
+    
+    cos[index[2], index[1], index[0]] = A * np.cos(k * cellsize[i_comp] * np.arange(0,N))
+
+    if i_comp == j_comp:
+        strain_anal[j_comp,...] = k * cos
+    else:
+        strain_anal[2 + i_comp + j_comp,...] = 0.5 * k * cos
+    
+    B_num = magnet.magnetoelastic_field.eval()
+    B_anal = np.zeros(shape=B_num.shape)
 
     m = magnet.magnetization.eval()
     for i in range(3):
         ip1 = (i+1)%3
         ip2 = (i+2)%3
 
-        h_anal[i,...] = - 2 * (B1 * strain_anal[i,...] * m[i,...] + 
-                               B2 * (strain_num[i+ip1+2,...] * m[ip1,...] + 
-                               strain_num[i+ip2+2,...] * m[ip2,...])) / msat
+        B_anal[i,...] = - 2  / msat * (B1 * strain_anal[i,...] * m[i,...] + 
+                        B2 * (strain_num[i+ip1+2,...] * m[ip1,...] + 
+                              strain_num[i+ip2+2,...] * m[ip2,...]))
 
-    assert max_semirelative_error(h_num, h_anal) < RTOL
+    assert max_semirelative_error(B_num, B_anal) < RTOL
 
-def test_x_sinx_cosx_0_B1():
-    d_comp, m_comp = 0, 0
+def test_x_Exx_B1():
+    m_comp, i, j = 0, 0, 0
     B1, B2 = B, 0
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+1)%3, B1, B2)
+    magnet = create_magnet(i, m_comp)
+    sine_displacement(magnet, i, j, B1, B2)
 
-def test_x_sinx_0_cosx_B1():
-    d_comp, m_comp = 0, 0
+def test_y_Eyy_B1():
+    m_comp, i, j = 1, 1, 1
     B1, B2 = B, 0
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+2)%3, B1, B2)
+    magnet = create_magnet(i, m_comp)
+    sine_displacement(magnet, i, j, B1, B2)
 
-def test_x_sinx_cosx_0_B2():
-    d_comp, m_comp = 0, 0
-    B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+1)%3, B1, B2)
-
-def test_x_cosz_0_sinz_B2():
-    d_comp, m_comp = 2, 0
-    B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+1)%3, B1, B2)
-
-def test_x_sinx_0_cosx_B2():
-    d_comp, m_comp = 0, 0
-    B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+2)%3, B1, B2)
-
-def test_x_cosy_siny_0_B2():
-    d_comp, m_comp = 1, 0
-    B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+2)%3, B1, B2)
-
-def test_y_0_siny_cosy_B1():
-    d_comp, m_comp = 1, 1
+def test_z_Ezz_B1():
+    m_comp, i, j = 2, 2, 2
     B1, B2 = B, 0
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+1)%3, B1, B2)
+    magnet = create_magnet(i, m_comp)
+    sine_displacement(magnet, i, j, B1, B2)
 
-def test_y_cosy_siny_0_B1():
-    d_comp, m_comp = 1, 1
-    B1, B2 = B, 0
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+2)%3, B1, B2)
-
-def test_y_sinx_cosx_0_B2():
-    d_comp, m_comp = 0, 1
+def test_y_Exy_B2():
+    m_comp, i, j = 1, 0, 1
     B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+1)%3, B1, B2)
+    magnet = create_magnet(i, m_comp)
+    sine_displacement(magnet, i, j, B1, B2)
 
-def test_y_0_siny_cosy_B2():
-    d_comp, m_comp = 1, 1
+def test_z_Exz_B2():
+    m_comp, i, j = 2, 0, 2
     B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+1)%3, B1, B2)
+    magnet = create_magnet(i, m_comp)
+    sine_displacement(magnet, i, j, B1, B2)
 
-def test_y_cosy_siny_0_B2():
-    d_comp, m_comp = 1, 1
+def test_x_Exy_B2():
+    m_comp, i, j = 0, 0, 1
     B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+2)%3, B1, B2)
+    magnet = create_magnet(i, m_comp)
+    sine_displacement(magnet, i, j, B1, B2)
 
-def test_y_0_cosz_sinz_B2():
-    d_comp, m_comp = 2, 1
+def test_z_Eyz_B2():
+    m_comp, i, j = 2, 1, 2
     B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+2)%3, B1, B2)
+    magnet = create_magnet(i, m_comp)
+    sine_displacement(magnet, i, j, B1, B2)
 
-def test_z_cosz_0_sinz_B1():
-    d_comp, m_comp = 2, 2
-    B1, B2 = B, 0
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+1)%3, B1, B2)
-
-def test_z_0_cosz_sinz_B1():
-    d_comp, m_comp = 2, 2
-    B1, B2 = B, 0
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+2)%3, B1, B2)
-
-def test_z_0_siny_cosy_B2():
-    d_comp, m_comp = 1, 2
+def test_x_Exz_B2():
+    m_comp, i, j = 0, 0, 2
     B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+1)%3, B1, B2)
+    magnet = create_magnet(i, m_comp)
+    sine_displacement(magnet, i, j, B1, B2)
 
-def test_z_cosz_0_sinz_B2():
-    d_comp, m_comp = 2, 2
+def test_y_Eyz_B2():
+    m_comp, i, j = 1, 1, 2
     B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+1)%3, B1, B2)
-
-def test_z_sinx_0_cosx_B2():
-    d_comp, m_comp = 0, 2
-    B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+2)%3, B1, B2)
-
-def test_z_0_cosz_sinz_B2():
-    d_comp, m_comp = 2, 2
-    B1, B2 = 0, B
-    world, magnet = create_magnet(d_comp, m_comp)
-    sine_displacement(magnet, d_comp, (d_comp+2)%3, B1, B2)
+    magnet = create_magnet(i, m_comp)
+    sine_displacement(magnet, i, j, B1, B2)
