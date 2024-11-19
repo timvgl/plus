91e3358eaec54283b0d696050ef0033f2881007d
DiegoDeGusem <diego.degusem@ugent.be>
2024-11-19 09:57:38 +0100

    Add correct dispersion example

--- examples/magnetoelastic_dispersion_afm.py
+++ examples/magnetoelastic_dispersion_afm.py
@@ -1,131 +1,162 @@
-import matplotlib.pyplot as plt
+"""This script creates the magnetoelastic dispersion relation when the 
+wave propagation and the magnetization form an angle theta as described in
+Magnetoelastic Waves in Thin Films.
+https://arxiv.org/abs/2003.12099
+
+This script will take a few minutes, then save the data.
+"""
+
 import numpy as np
 from tqdm import tqdm
-from mumaxplus import Antiferromagnet, Grid, World
-from mumaxplus.util.constants import GAMMALL
+import matplotlib.pyplot as plt
+from mumaxplus import World, Grid, Antiferromagnet
+import os.path
+from mumaxplus.util.constants import *
 
-# Antiferromagnetic spinwave dispersion relation
+# angle between magnetization and wave propagation
+theta = np.pi/6
 
-# Numerical parameters
-fmax = 8E12          # maximum frequency (in Hz) of the sinc pulse
-T = 1E-11            # simulation time (longer -> better frequency resolution)
-dt = 1 / (2 * fmax)  # the sample time
-dx = 1E-10           # cellsize
-nx = 5120           # number of cells
-k = np.linspace(-np.pi/dx, np.pi/dx, 2000)
-
-# Material/system parameters
-Bz = 4           # bias field along the z direction
-A = 2.48e-12         # exchange constant
+# magnet parameters
+msat = 566e3
+a = 0.61e-9
+aex = 2.48e-12
+A_c = -9.93e5 * a**2
 A_nn = 0
-A_c = -1E-12
-Ms = 566e3         # saturation magnetization
-alpha = 0.005      # damping parameter
-gamma = GAMMALL    # gyromagnetic ratio
-K = 4.64631223e-25 / 1.12152364e-28
-mu0 = 1.256637062E-6
-latcon = 0.35e-9
+K = 611e3
+alpha = 2e-3
+Bdc = 2
 
 # magnetoelastic parameters
-rho = 2800  # mass density MnPS3
-B2 = -1.2e6
-B1 = B2
-c11 = 245e9*2
-c44 = 75e9*2
+rho = 2800
+B = -55e6
+B1 = B
+B2 = B
+c11 = 200e9
+c44 = 70e9
 c12 = c11 - 2*c44  # assume isotropic
+eta = 2e11
 
-# Create the world
-grid_size = (nx, 1, 1)
-cell_size = (dx, dx, dx)
+# time settings
+fmax = 5e12/(2*np.pi)        # maximum frequency (in Hz) of the sinc pulse
+time_max = 4e-10             # simulation time (longer -> better frequency resolution)
+dt = 1 / (2 * fmax)          # optimal sample time
+nt = 1 + int(time_max / dt)  # number of time points
 
-world = World(cell_size)
-world.bias_magnetic_field = (0, 0, Bz)
+# simulation grid parameters
+nx, ny, nz = 2048, 1, 1
+# cellsize should stay smaller than exchange length
+# and much smaller than the smallest wavelength
+cx, cy, cz = 1e-9, 1e-9, 1e-9
 
-# Create a ferromagnet
-magnet = Antiferromagnet(world, Grid(size=grid_size))
-for sub in magnet.sublattices:
-    sub.msat = Ms
-    sub.aex = A
-    sub.alpha = alpha
-    sub.B1 = B1
-    sub.B2 = B2
-    sub.ku1 = K
-    sub.anisU = (0,0,1)
+# file names
+m_filename = "magnon-phonon_magnetizations_AFM.npy"
+u_filename = "magnon-phonon_displacements_AFM.npy"
 
-magnet.afmex_nn = A_nn
-magnet.afmex_cell = A_c
-magnet.latcon = latcon
+def simulation(theta):
+    # create a world and a 1D magnet with PBC in x and y
+    cellsize = (cx, cy, cz)
+    grid = Grid((nx, ny, nz))
+    world = World(cellsize, mastergrid=Grid((nx,0,0)), pbc_repetitions=(2,0,0))
+    magnet = Antiferromagnet(world, grid)
 
-Bt = lambda t: (1e2* np.sinc(2 * fmax * (t - T / 2)), 0, 0)
-mask = np.zeros(shape=(1, 1, nx))
-# Put signal at the center of the simulation box
-mask[:, :, nx // 2 - 1:nx // 2 + 1] = 1
-for sub in magnet.sublattices:
-    sub.bias_magnetic_field.add_time_term(Bt, mask)
+    magnet.enable_demag = False
 
-magnet.sub1.magnetization = (0, 0, 1)
-magnet.sub2.magnetization = (0, 0, -1)
-#magnet.minimize()
+    # magnet parameters without magnetoelastics
+    for sub in magnet.sublattices:
+        sub.msat = msat
+        sub.aex = aex
+        sub.alpha = alpha
+        sub.ku1 = K
+        sub.anisU = (np.cos(theta), np.sin(theta), 0)
+    magnet.afmex_cell = A_c
+    magnet.afmex_nn = A_nn
+    magnet.latcon = a
+    magnet.sub1.magnetization = (np.cos(theta), np.sin(theta), 0)
+    magnet.sub2.magnetization = (-np.cos(theta), -np.sin(theta), 0)
+    magnet.bias_magnetic_field = (Bdc*np.cos(theta), Bdc*np.sin(theta), 0)
 
-magnet.c11 = c11
-magnet.c12 = c12
-magnet.c44 = c44
-magnet.rho = rho
+    print("Minimizing...")
+    magnet.minimize()
+    print("Minimized!")
 
-magnet.enable_elastodynamics = True
-# time stepping
-world.timesolver.adaptive_timestep = False
-world.timesolver.timestep = 1e-15
+    # elastic parameters
+    magnet.enable_elastodynamics = True
+    for sub in magnet.sublattices:
+        sub.B1 = B1
+        sub.B2 = B2
+    magnet.rho = rho
+    magnet.c11 = c11 
+    magnet.c44 = c44
+    magnet.c12 = c12
 
-# parameters to save
-nt = 1 + int(T / dt)
+    # no displacement initially
+    magnet.elastic_displacement = (0, 0, 0)
 
-m = np.zeros(shape=(nt, 3, 1, 1, nx))
-u = np.zeros(shape=(nt, 3, 1, 1, nx))
+    # damping
+    magnet.sub1.alpha = alpha
+    magnet.sub2.alpha = alpha
+    magnet.eta = eta
 
-# run a while and save the data
-m[0,...] = magnet.sub1.magnetization.eval()
-u[0,...] = magnet.elastic_displacement.eval()
-for i in tqdm(range(1, nt)):
-    world.timesolver.run(dt)
-    m[i,...] = magnet.sub1.magnetization.eval()
-    u[i,...] = magnet.elastic_displacement.eval()
+    # time stepping
+    world.timesolver.adaptive_timestep = False
+    world.timesolver.timestep = dt/100
 
+    # parameters to save
+    m = np.zeros(shape=(nt, 3, nz, ny, nx))
+    u = np.zeros(shape=(nt, 3, nz, ny, nx))
+    
+    # add magnetic field and external force excitation in the middle of the magnet
+    Fac = 1e16  # force pulse strength
+    Bac = 1e3  # magnetic pulse strength
+
+    mask = np.zeros(shape=(1, 1, nx))
+    # Put signal at the center of the simulation box
+    mask[:, :, nx // 2 - 1:nx // 2 + 1] = 1
+    Fac_dir = np.array([Fac, Fac, Fac])/np.sqrt(3)
+    Bac_dir = np.array([Bac, Bac, Bac])/np.sqrt(3)
+    def time_force_field(t):
+        sinc = np.sinc(2 * fmax * (t - time_max / 2))
+        return tuple(sinc * Fac_dir)
+    def time_magnetic_field(t):
+        sinc = np.sinc(2 * fmax * (t - time_max / 2))
+        return tuple(sinc * Bac_dir)
+    magnet.external_body_force.add_time_term(time_force_field, mask=mask)
+    magnet.bias_magnetic_field.add_time_term(time_magnetic_field, mask=mask)
+
+    # run a while and save the data
+    m[0,...] = magnet.sub1.magnetization.eval()
+    u[0,...] = magnet.elastic_displacement.eval()
+    for i in tqdm(range(1, nt)):
+        world.timesolver.run(dt)
+        m[i,...] = magnet.sub1.magnetization.eval()
+        u[i,...] = magnet.elastic_displacement.eval()
+        
+    np.save(m_filename, m)
+    np.save(u_filename, u)
+    return m, u
+
+# check if the files already exist
+if os.path.isfile(m_filename):
+    m = np.load(m_filename)
+    u = np.load(u_filename)
+else:
+    m, u = simulation(theta)
+
+print(m[-1,...])
 # plotting ranges
-xmin, xmax = 50e6, 5000e6  # rad/m
-ymin, ymax = 50e9, 8000e9  # Hz
-extent = [-(2 * np.pi) / (2 * dx) * (nx+1)/nx,
-          (2 * np.pi) / (2 * dx) * (nx-1)/nx,
-          -1 / (2 * dt) * nt/(nt-1),
-          1 / (2 * dt) * nt/(nt-1)]
+xmin, xmax = -0.6, 0.6  # rad/nm
+ymin, ymax = 0.2/(2*np.pi), 2.5/(2*np.pi)  # THz
+extent = [-(2 * np.pi) / (2 * cx) * (nx+1)/nx * 1e-9,
+          (2 * np.pi) / (2 * cx) * (nx-1)/nx * 1e-9,
+          -1 / (2 * dt) * nt/(nt-1) * 1e-12,
+          1 / (2 * dt) * nt/(nt-1) * 1e-12]
 
-def plot_FT(FT, name=""):
-    maximum = np.max(FT[y_start:y_end,x_start:x_end])
-    FT /= maximum
-    plt.imshow(np.abs(FT), extent=extent, aspect='auto', origin='lower', cmap="inferno", vmin=0, vmax=0.1)
-    plt.title(f"{name} {maximum}")
-
-    # magnon wave
-    mu0Ms = mu0 * Ms
-    He = -4 * A_c / (latcon*latcon*mu0Ms)
-    Ha = 2 * K / mu0Ms
-    Hint = (2*A - A_nn) / mu0Ms
-    omega_theory = mu0 * gamma * np.sqrt((2*He + Ha + Hint * k**2) * (Ha + Hint * k**2)) + Bz * gamma
-    plt.plot(k, omega_theory/(2*np.pi), lw=1)
-    # elastic waves
-    vt = np.sqrt(c44/rho)
-    vl = np.sqrt(c11/rho)
-    omega_t = np.abs(vt*k)
-    omega_l = np.abs(vl*k)
-    plt.plot(k, omega_t/(2*np.pi))
-    plt.plot(k, omega_l/(2*np.pi))
-
-    plt.xlim((xmin, xmax))
-    plt.ylim((ymin, ymax))
-    plt.colorbar()
-    plt.savefig(f"{name}.png")
-    plt.close()
-
+# Fourier in time and x-direction of displacement and magnetization
+u_FT = np.zeros((nt, nx))
+m_FT = np.zeros((nt, nx))
+for i in range(3):
+    u_FT += np.abs(np.fft.fftshift(np.fft.fft2(u[:,i,0,0,:])))
+    m_FT += np.abs(np.fft.fftshift(np.fft.fft2(m[:,i,0,0,:])))
 
 # normalize them in the relevant area, so they are visible in the plot
 x_start = int((xmin - extent[0]) / (extent[1] - extent[0]) * nx)
@@ -133,53 +164,74 @@ x_end = int((xmax - extent[0]) / (extent[1] - extent[0]) * nx)
 y_start = int((ymin - extent[2]) / (extent[3] - extent[2]) * nt)
 y_end = int((ymax - extent[2]) / (extent[3] - extent[2]) * nt)
 
-# Apply the two dimensional FFT
-# Fourier in time and x-direction of displacement and magnetization
-u_FT = np.zeros((nt, nx))
-m_FT = np.zeros((nt, nx))
-param = ["x", "y", "z"]
-for i in range(3):
-    u_FT += np.abs(np.fft.fftshift(np.fft.fft2(u[:,i,0,0,:])))
-    m_FT += np.abs(np.fft.fftshift(np.fft.fft2(m[:,i,0,0,:])))
-    plot_FT(np.abs(np.fft.fftshift(np.fft.fft2(u[:,i,0,0,:]))), f"u{param[i]}")
-    plot_FT(np.abs(np.fft.fftshift(np.fft.fft2(m[:,i,0,0,:]))), f"m{param[i]}")
-
 u_max = np.max(u_FT[y_start:y_end, x_start:x_end])
 m_max = np.max(m_FT[y_start:y_end, x_start:x_end])
 
 FT_tot = u_FT/u_max + m_FT/m_max
 
-plt.figure(figsize=(10, 6))
+# numerical calculations
+lambda_exch = (2*aex) / (MU0*msat**2)
+k = np.linspace(xmin*1e9, xmax*1e9, 2000)
 
-# Show the intensity plot of the 2D FFT
-plt.imshow(np.abs(FT_tot)**2, extent=extent, aspect='auto', origin='lower', cmap="inferno", vmin=0, vmax=2)
+fig, ax = plt.subplots()
+linewidth = 2
 
-# Plot the analytical derived dispersion relation
 # elastic waves
 vt = np.sqrt(c44/rho)
 vl = np.sqrt(c11/rho)
-omega_t = np.abs(vt*k)
-omega_l = np.abs(vl*k)
-plt.plot(k, omega_t/(2*np.pi))
-plt.plot(k, omega_l/(2*np.pi))
+w_t = np.abs(vt*k)
+w_l = np.abs(vl*k)
+ax.plot(k*1e-9, w_t/(2*np.pi)*1e-12, color="red", lw=linewidth, label="elastic")
+ax.plot(k*1e-9, w_l/(2*np.pi)*1e-12, color="red", lw=linewidth)
+ax.plot(k*1e-9, -w_t/(2*np.pi)*1e-12, color="red", lw=linewidth)
+ax.plot(k*1e-9, -w_l/(2*np.pi)*1e-12, color="red", lw=linewidth)
 
-# magnon wave
-mu0Ms = mu0 * Ms
-He = -4 * A_c / (latcon*latcon*mu0Ms)
-Ha = 2 * K / mu0Ms
-Hint = (2*A - A_nn) / mu0Ms
-omega_theory = mu0 * gamma * np.sqrt((2*He + Ha + Hint * k**2) * (Ha + Hint * k**2)) + Bz * gamma
-plt.plot(k, omega_theory/(2*np.pi), lw=1)
+# spin waves
+w_ext = GAMMALL * Bdc
+w_ani = GAMMALL * 2*K / msat
+w_ex = GAMMALL * 2*aex/msat * k**2
+w_c = GAMMALL * 4*A_c/(a**2 * msat)
+w_nn = GAMMALL * A_nn/msat * k**2
 
-plt.axhline(gamma * Bz / (2 * np.pi), c='g', ls='--', lw=1)
-plt.xlim([-np.pi/dx, np.pi/dx])
-plt.ylim([0, fmax])
-plt.vlines([-1/dx, 1/dx], 0, fmax, 'r', '--', lw=1)
-plt.ylabel("$f$ (Hz)")
-plt.xlabel("$k$ (1/m)")
-plt.colorbar()
-plt.xlim((xmin, xmax))
-plt.ylim((ymin, ymax))
-plt.title('Antiferromagnetic Spinwave Dispersion Relation')
-plt.savefig("tot.png")
+# Magnon waves
+w_mag = np.sqrt((w_ani + w_ex - w_nn)*(w_ani + w_ex - 2*w_c + w_nn))
+omega_magn1 = w_mag + w_ext
+omega_magn2 = w_mag - w_ext
+omega_magn3 = -w_mag + w_ext
+omega_magn4 = -w_mag - w_ext
+ax.plot(k*1e-9, omega_magn1/(2*np.pi)*1e-12, color="green", lw=linewidth, label="magnetic")
+ax.plot(k*1e-9, omega_magn2/(2*np.pi)*1e-12, color="green", lw=linewidth)
+ax.plot(k*1e-9, omega_magn3/(2*np.pi)*1e-12, color="green", lw=linewidth)
+ax.plot(k*1e-9, omega_magn4/(2*np.pi)*1e-12, color="green", lw=linewidth)
+
+# Magnon-Phonon waves
+J = GAMMALL * B**2 / (rho*msat)
+w = np.linspace(ymin*(2*np.pi)*1e12, ymax*(2*np.pi)*1e12, 2000)
+k, w = np.meshgrid(k,w)
+
+w_g = w_ani + w_ex - 2*w_c + w_nn
+omega_mp = 2*J*k**2 * w_g*np.cos(theta)**2 *(J*k**2 * w_g*(w_l**2+w_t**2-2*w**2)\
+                      - (w_l**2-w**2)*(w_t**2-w**2)*(w_mag**2 - w_ext**2-w**2)\
+                      + J*k**2 * w_g*(w_l**2-w_t**2)*np.cos(4*theta))
+
+omega_mp += (w_t**2-w**2)*(-J*k**2 * w_g*(w_l**2+w_t**2-2*w**2)*(w_mag**2-w_ext**2-w**2)\
+            +(w_l**2-w**2)*(w_t**2-w**2)*(w_mag**2-(w_ext-w)**2)*(w_mag**2-(w_ext+w)**2)\
+            -J*k**2 * w_g*(w_l**2-w_t**2)*(w_mag**2-w_ext**2-w**2)*np.cos(4*theta))
+
+contour = ax.contour(k*1e-9, w/(2*np.pi)*1e-12, omega_mp, [0], colors="white",
+                     linewidths=linewidth)
+
+ax.plot([], [], label="magnetoelastic", color="white")  # for legend entry
+
+# plot numerical result
+ax.imshow(FT_tot**2, aspect='auto', origin='lower', extent=extent,
+           vmin=0, vmax=0.6, cmap="inferno")
+
+# plot cleanup
+ax.set_xlim(xmin, xmax)
+ax.set_ylim(ymin, ymax)
+ax.set_xlabel("wavenumber (rad/nm)")
+ax.set_ylabel("frequency (THz)")
+ax.set_title("Antiferromagnetoelastic dispersion relation")
+ax.legend(loc="lower right")
 plt.show()
