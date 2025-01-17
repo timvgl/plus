2f4cf7ba708fdc8330709419ad209e6f91f91b90
DiegoDeGusem <diego.degusem@ugent.be>
2025-01-17 11:07:53 +0100

    Add a test for negative strains, made magnetization random and raised RTOL

--- test/mumax3/test_mumax3_rigid_strain.py
+++ test/mumax3/test_mumax3_rigid_strain.py
@@ -4,7 +4,7 @@ from mumax3 import Mumax3Simulation
 from mumaxplus import Ferromagnet, Grid, World
 from mumaxplus.util import formulary
 
-RTOL = 1e-7  # floating point :)
+RTOL = 3e-5
 strain = 1e-4
 
 def max_relative_error(result, wanted):
@@ -16,7 +16,6 @@ def simulation(exx, eyy, ezz, exy, exz, eyz):
     # arbitrarily chosen parameters
     msat, aex, alpha = 566e3, 2.48e-12, 0.02
     B = -55e6
-    magnetization = (1,1,1)
     cellsize = (1e-9, 2e-9, 3.2e-9)
     gridsize = (30, 16, 4)
 
@@ -36,7 +35,8 @@ def simulation(exx, eyy, ezz, exy, exz, eyz):
             B1 = {B}
             B2 = {B}
 
-            m = Uniform{tuple(magnetization)}
+            m = randommag()
+            saveas(m, "m.ovf")
             saveas(B_mel, "B.ovf")
         """
     )
@@ -46,7 +46,7 @@ def simulation(exx, eyy, ezz, exy, exz, eyz):
     magnet.msat = msat
     magnet.aex = aex
     magnet.alpha = alpha
-    magnet.magnetization = magnetization
+    magnet.magnetization.set(mumax3sim.get_field("m"))
     magnet.rigid_norm_strain = (exx, eyy, ezz)
     magnet.rigid_shear_strain = (exy, exz, eyz)
     magnet.B1 = B
@@ -58,7 +58,6 @@ def simulation(exx, eyy, ezz, exy, exz, eyz):
 @pytest.mark.mumax3
 class TestRigidStrain:
     """Test rigid strain against mumax³."""
-
     def test_exx(self):
         exx, eyy, ezz, exy, exz, eyz = strain, 0, 0, 0, 0, 0
         magnet, mumax3sim = simulation(exx, eyy, ezz, exy, exz, eyz)
@@ -103,6 +102,15 @@ class TestRigidStrain:
 
     def test_eyz(self):
         exx, eyy, ezz, exy, exz, eyz = 0, 0, 0, 0, 0, strain
+        magnet, mumax3sim = simulation(exx, eyy, ezz, exy, exz, eyz)
+        wanted = mumax3sim.get_field("B")
+        result = magnet.magnetoelastic_field.eval()
+        err = max_relative_error(result, wanted)
+        assert err < RTOL
+
+    def test_combined(self):
+        exx, eyy, ezz = -strain, -strain, -strain
+        exy, exz, eyz = -strain, -strain, -strain
         magnet, mumax3sim = simulation(exx, eyy, ezz, exy, exz, eyz)
         wanted = mumax3sim.get_field("B")
         result = magnet.magnetoelastic_field.eval()
