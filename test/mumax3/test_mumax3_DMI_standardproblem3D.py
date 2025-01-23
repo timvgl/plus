5d743b5055d0c8143b5ff04590058bd118d6246c
DiegoDeGusem <diego.degusem@ugent.be>
2025-01-23 13:36:02 +0100

    Add the 3D DMI stdp

---
+++ test/mumax3/test_mumax3_DMI_standardproblem3D.py
@@ -0,0 +1,103 @@
+"""This test is based on the 3D case in
+   https://iopscience.iop.org/article/10.1088/1367-2630/aaea1c"""
+
+import pytest
+import numpy as np
+from mumaxplus import Ferromagnet, Grid, World
+from mumax3 import Mumax3Simulation
+from mumaxplus.util.shape import *
+from mumaxplus.util.config import *
+
+
+ATOL = 2e-3
+def max_relative_error(result, wanted):
+    err = np.linalg.norm(result - wanted, axis=0)
+    return np.max(err)
+
+def simulations():
+    """This simulates a 3D cylinder with bulk DMI and a bloch skyrmion
+       in both mumax³ and mumax⁺."""
+    
+    # constants
+    A = 8.78e-12
+    D = 1.5e-3
+    Ms = 0.384e6
+    Bz = 0.4
+
+    diam, thickness = 183e-9, 21e-9
+    charge, pol = 1, -1
+
+    gridsize = (183, 183, 21)
+    cellsize = (1e-9, 1e-9, 1e-9)
+
+    world = World(cellsize=cellsize)
+    geo = Cylinder(diam, thickness).translate(91.5e-9 - 0.5e-9, 91.5e-9 - 0.5e-9, 10.5e-9 - 0.5e-9)
+    magnet = Ferromagnet(world, Grid(gridsize), geometry=geo)
+
+    magnet.enable_demag = False
+    magnet.msat = Ms
+    magnet.aex = A
+    magnet.dmi_tensor.set_bulk_dmi(D)
+
+    magnet.bias_magnetic_field = (0,0,Bz)
+
+    magnet.magnetization = blochskyrmion(magnet.center, 91.5e-9, charge, pol)
+
+    tolerance = 1e-6
+    magnet.minimize(tolerance)
+
+    # simulation mumax³
+    mumax3sim = Mumax3Simulation(
+        f"""
+            nx := 183
+            ny := 183
+            nz := 21
+
+            lx := 183e-9
+            ly := 183e-9
+            lz := 21e-9
+
+            dx := lx / nx
+            dy := ly / ny
+            dz := lz / nz
+
+            SetGridSize(nx, ny, nz)
+            SetCellSize(dx, dy, dz)
+
+            // Define the cylinder
+            SetGeom(Circle(lx))
+
+            Msat         = {Ms}
+            Aex          = {A}
+            Dbulk        = {D}
+
+            // External field in T
+            B_ext = vector(0, 0, {Bz})
+
+            // No Demag
+            NoDemagSpins = 1
+
+            m = BlochSkyrmion(1, -1)
+
+            OutputFormat = OVF2_TEXT
+
+            // Relax with conjugate gradient:
+            MinimizerStop = {tolerance}
+            minimize();
+            SaveAs(m, "m")
+        """
+    )
+
+    return  magnet, mumax3sim
+
+
+@pytest.mark.mumax3
+@pytest.mark.slow
+class TestDMI3D:
+    """Compare the results of the simulations by comparing the magnetizations.
+    """
+
+    def test(self):
+        magnet, mumax3sim = simulations()
+        err = max_relative_error(magnet.magnetization.eval(), mumax3sim.get_field("m"))
+        assert err < ATOL
